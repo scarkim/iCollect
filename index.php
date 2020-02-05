f5c4681b37bc1a5a6a727ef6ff10e10aaa432ab7
Scarlett Kim <scarlettkimm@gmail.com>
2020-02-05 17:12:57 -0500

    add starter files composer.json, index.php, and htaccess

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+//Start a session
+session_start();
+
+//Turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+//Required file
+require_once('vendor/autoload.php');
+require_once('model/validate.php');
+
+//Instantiate Fat-Free
+$f3 = Base::instance();
+
+//Turn on Fat-Free error reporting
+$f3->set('DEBUG', 3);
+$f3->route("GET /", function (){
+
+});
+//Run Fat-Free
+$f3->run();

3f613df08b965283162b04da5a59ca207a56f8c4
Robert Cox <rcox18@mail.greenriver.edu>
2020-02-08 16:00:16 -0500

    moved connection.php location and changed require path

--- connection.php
+++
@@ -1,7 +0,0 @@
-<?php
-$username = "rcoxgree_grcuser";
-$password = "rcoxgree_grcuser";
-$hostname = "localhost";
-$database = "rcoxgree_icollect";
-
-$cnxn = mysqli_connect($hostname, $username, $password, $database);
--- index.php
+++ index.php
@@ -8,7 +8,7 @@ error_reporting(E_ALL);
 
 //Required file
 require_once('vendor/autoload.php');
-require ("connection.php");
+require ("../../../connection.php");
 require_once('model/validate.php');
 
 //Instantiate Fat-Free

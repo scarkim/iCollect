952d16f13d9e152bb2125d0c3f3702018b837602
Robert Cox <rcox18@mail.greenriver.edu>
2020-03-06 12:50:59 -0500

    just for pull

--- scripts/js-validation.js
+++ scripts/js-validation.js
@@ -1,88 +1,113 @@
-if (document.title === "iCollect Signup" || document.title === "iCollect Login") {
+if (document.title === "iCollect Signup" ||
+    document.title === "iCollect Login" ||
+    document.title === "Create Collection") {
 
     let form = document.getElementsByTagName("form");
+    if (document.title === "iCollect Signup" ||
+        document.title === "iCollect Login") {
+        let errUserName = document.getElementById("err-username");
+        let errPassword = document.getElementById("err-password");
 
-    let errUserName = document.getElementById("err-username");
-    let errPassword = document.getElementById("err-password");
+        errUserName.style.visibility = "hidden";
+        errPassword.style.visibility = "hidden";
 
-    errUserName.style.visibility = "hidden";
-    errPassword.style.visibility = "hidden";
+        if (document.title === "iCollect Signup") {
 
-    if (document.title === "iCollect Signup") {
+            let errEmail = document.getElementById("err-email");
+            let errPassword2 = document.getElementById("err-password2");
+            let errAcctType = document.getElementById("err-acct-type");
 
-        let errEmail = document.getElementById("err-email");
-        let errPassword2 = document.getElementById("err-password2");
-        let errAcctType = document.getElementById("err-acct-type");
-
-        errEmail.style.visibility = "hidden";
-        errPassword2.style.visibility = "hidden";
-        errAcctType.style.visibility = "hidden";
-        form[0].onsubmit = validateSignup;
-
-
-        function validateSignup() {
-
-            errUserName.style.visibility = "hidden";
             errEmail.style.visibility = "hidden";
-            errPassword.style.visibility = "hidden";
             errPassword2.style.visibility = "hidden";
             errAcctType.style.visibility = "hidden";
+            form[0].onsubmit = validateSignup;
 
-            let username = document.getElementById("username").value;
-            let password = document.getElementById("password").value;
-            let email = document.getElementById("email").value;
-            let password2 = document.getElementById("password2").value;
-            let acctType = document.getElementsByName("accountType");
 
-            let isValid = true;
+            function validateSignup() {
 
-            if (username === "" || !username.match(/^[0-9a-zA-Z]+$/)) {
+                errUserName.style.visibility = "hidden";
+                errEmail.style.visibility = "hidden";
+                errPassword.style.visibility = "hidden";
+                errPassword2.style.visibility = "hidden";
+                errAcctType.style.visibility = "hidden";
+
+                let username = document.getElementById("username").value;
+                let password = document.getElementById("password").value;
+                let email = document.getElementById("email").value;
+                let password2 = document.getElementById("password2").value;
+                let acctType = document.getElementsByName("accountType");
+
+                let isValid = true;
+
+                if (username === "" || !username.match(/^[0-9a-zA-Z]+$/)) {
                     errUserName.style.visibility = "visible";
                     isValid = false;
-            }
+                }
 
-            if (password === "") {
-                errPassword.style.visibility = "visible";
-                isValid = false;
-            } else if (password !== password2) {
-                errPassword2.style.visibility = "visible";
-                isValid = false;
-            }
-
-            if (email === "" || !email.match(/^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/)) {
-                errEmail.style.visibility = "visible";
-                isValid = false;
-            }
-
-            for(let i = 0; i < acctType.length; i++) {
-                if(acctType[i].value !== "0" && acctType[i].value !== "1") {
-                    errAcctType.style.visibility = "visible";
+                if (password === "") {
+                    errPassword.style.visibility = "visible";
+                    isValid = false;
+                } else if (password !== password2) {
+                    errPassword2.style.visibility = "visible";
                     isValid = false;
                 }
+
+                if (email === "" || !email.match(/^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/)) {
+                    errEmail.style.visibility = "visible";
+                    isValid = false;
+                }
+
+                for(let i = 0; i < acctType.length; i++) {
+                    if(acctType[i].value !== "0" && acctType[i].value !== "1") {
+                        errAcctType.style.visibility = "visible";
+                        isValid = false;
+                    }
+                }
+
+                return isValid;
             }
+        } else if (document.title === "iCollect Login") {
+            form[0].onsubmit = validateLogin;
 
-            return isValid;
+            function validateLogin() {
+
+                errUserName.style.visibility = "hidden";
+                errPassword.style.visibility = "hidden";
+
+                let username = document.getElementById("username").value;
+                let password = document.getElementById("password").value;
+
+                let isValid = true;
+
+                if (username === "" || !username.match(/^[0-9a-zA-Z]+$/)) {
+                    errUserName.style.visibility = "visible";
+                    isValid = false;
+                }
+
+                if (password === "") {
+                    errPassword.style.visibility = "visible";
+                    isValid = false;
+                }
+
+                return isValid;
+            }
         }
-    } else if (document.title === "iCollect Login") {
-        form[0].onsubmit = validateLogin;
+    } else if (document.title === "Create Collection") {
 
-        function validateLogin() {
+        form[0].onsubmit = validateCollection();
 
-            errUserName.style.visibility = "hidden";
-            errPassword.style.visibility = "hidden";
+        function validateCollection() {
 
-            let username = document.getElementById("username").value;
-            let password = document.getElementById("password").value;
-
+            let collectionTitle = document.getElementById("title").value;
+            let collectionDescription = document.getElementById("description").value;
+            let collectionTitleErr = document.getElementById("collectionTitleErr");
+            let collectionDescriptionErr = document.getElementById("collectionDescriptionErr");
+            collectionTitleErr.innerText = "";
+            collectionDescriptionErr.innerText = "";
             let isValid = true;
 
-            if (username === "" || !username.match(/^[0-9a-zA-Z]+$/)) {
-                errUserName.style.visibility = "visible";
-                isValid = false;
-            }
-
-            if (password === "") {
-                errPassword.style.visibility = "visible";
+            if (collectionTitle.length > 50) {
+                collectionTitleErr.innerText = "too long, must be less than 50";
                 isValid = false;
             }
 

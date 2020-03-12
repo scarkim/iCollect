fc723e41e26403358456fef9cad99f3d2f9ad0e3
Robert Cox <rcox18@mail.greenriver.edu>
2020-03-12 12:50:50 -0400

    cancel prompt bug fix

--- scripts/table-row-clicker.js
+++ scripts/table-row-clicker.js
@@ -4,12 +4,15 @@ $('#table').bootstrapTable({
         /*alert(field +', '+ value +', '+ row[0] +', '+ $element[0]);*/
         let colName = document.getElementsByTagName("th")[field].innerText;
         let newValue = prompt("Change Item "+ row[0] + "\n" + colName +": "+ row[1] + "\n" + colName+":");
-        $.post(
-            "editTableAjax",
-            {itemID:row[0], oldValue:value, colName:colName, newValue:newValue},
-            function (result) {
-                $("#result").html(result);
-                window.location.reload();
-            });
+        if (newValue != null) {
+            $.post(
+                "editTableAjax",
+                {itemID:row[0], oldValue:value, colName:colName, newValue:newValue},
+                function (result) {
+                    $("#result").html(result);
+                    window.location.reload();
+                });
+        }
+
     }
 });

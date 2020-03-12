2768f10436528685bfd39b27d12b139fa543e8f0
Robert Cox <rcox18@mail.greenriver.edu>
2020-03-12 14:03:53 -0400

    green username available

--- scripts/iCollect-ajax.js
+++ scripts/iCollect-ajax.js
@@ -4,10 +4,19 @@ $(document).ready(function() {
 
     $("#username").keyup(function (e) {
         e.preventDefault();
+        $("#ajax-name-unavailable").text("");
+        $("#ajax-name-available").text("");
         $.post("signupAjax", { username: $("#username").val()},
             function (result)
             {
-                $("#ajax-name-result").text(result);
+
+
+                if(result === "Username taken" || result === "alpha-numeric only") {
+                    $("#ajax-name-unavailable").text(result);
+                } else {
+                    $("#ajax-name-available").text(result);
+                }
+
             }
         );
     });
